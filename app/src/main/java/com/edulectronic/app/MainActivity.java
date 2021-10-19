017d41e008f9cdbf8dbb3b17a5ac5ac0be24ef83
LAPTOP-QIQ4NRNI\LENOVO <darrukk01@gmail.com>
2021-10-19 09:10:08 +0700

    tombol daftar dan informasi, layout daftar pustaka(isi)

--- app/src/main/java/com/edulectronic/app/MainActivity.java
+++ app/src/main/java/com/edulectronic/app/MainActivity.java
@@ -2,13 +2,30 @@ package com.edulectronic.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button materi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        materi = (Button) findViewById(R.id.materi);
+        materi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opendaftarisi();
+
+            }
+        });
+    }
+    public void opendaftarisi(){
+        Intent intent = new Intent(this, daftarisi.class);
+        startActivity(intent);
     }
 }
